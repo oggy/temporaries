cbe5bdb81346038c9a77690920307706f21ac570
George Ogata <george.ogata@gmail.com>
2010-09-27 10:30:52 -0400

    Enter temporary directory during cuke scenarios.

--- features/support/env.rb
+++ features/support/env.rb
@@ -6,10 +6,15 @@ require 'spec/expectations'
 TMP = File.expand_path(File.dirname(__FILE__) + '/../tmp')
 LIB = File.expand_path(File.dirname(__FILE__) + '/../../lib')
 
+original_pwd = nil
+
 Before do
-  FileUtils.mkdir_p(TMP)
+  FileUtils.mkdir_p TMP
+  original_pwd = Dir.pwd
+  Dir.chdir TMP
 end
 
 After do
-  FileUtils.rm_rf(TMP)
+  Dir.chdir original_pwd
+  FileUtils.rm_rf TMP
 end
