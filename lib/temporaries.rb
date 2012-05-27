636b6333d61f430578e26beeafbd11a79648e0b7
George Ogata <george.ogata@gmail.com>
2012-05-26 22:24:01 -0400

    Support minitest/unit and minitest/spec.

--- lib/temporaries.rb
+++ lib/temporaries.rb
@@ -9,6 +9,8 @@ if defined?($TEMPORARIES_TEST)
   # Testing this library. Don't install anything.
 elsif defined?(RSpec)
   Temporaries::Adapters::RSpec.install
+elsif defined?(MiniTest::Unit::TestCase)
+  Temporaries::Adapters::MiniTest.install
 elsif defined?(Test::Unit)
   Temporaries::Adapters::TestUnit.install
 end
--- lib/temporaries/adapters.rb
+++ lib/temporaries/adapters.rb
@@ -1,6 +1,7 @@
 module Temporaries
   module Adapters
     autoload :Base, 'temporaries/adapters/base'
+    autoload :MiniTest, 'temporaries/adapters/mini_test'
     autoload :RSpec, 'temporaries/adapters/rspec'
     autoload :TestUnit, 'temporaries/adapters/test_unit'
   end
